c37efde9cdf60336bb80da4fef87b0846f8809c2
Bartłomiej Kuras <hash.deb@gmail.com>
2024-11-08 18:32:41 +0100

    WiP: PGN Setup

--- emily-cli/src/main.rs
+++ emily-cli/src/main.rs
@@ -5,16 +5,37 @@ use serde::Deserialize;
 use structopt::StructOpt;
 use tracing::{debug, error};
 
+mod repertoire;
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "emily-cli", about = "Chess assistant application")]
 struct Opt {
     /// Config file
     #[structopt(short, long, default_value = "config.toml")]
     config: PathBuf,
+
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Repertoire management
+    Rep,
 }
 
 #[derive(Debug, Deserialize)]
-struct Config;
+struct Config {
+    /// Path to the user data
+    #[serde(default = "Config::default_data")]
+    data: PathBuf,
+}
+
+impl Config {
+    fn default_data() -> PathBuf {
+        "data".into()
+    }
+}
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
